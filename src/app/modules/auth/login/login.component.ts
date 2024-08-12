8db929aeb5db0b4172d28010517d4962ee32272a
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-12 21:04:52 +0300

    added auth forms validation

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,35 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  user: FormGroup
+
+  constructor() {
+    this.user = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
+
+  submit() {
+    if (this.user.valid) {
+      console.log(this.user.value)
+    } else {
+      console.log('Not valid')
+    }
+  }
 
 }
--- src/app/modules/auth/registration/registration.component.ts
+++ src/app/modules/auth/registration/registration.component.ts
@@ -1,15 +1,44 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  user: FormGroup
 
+  constructor() {
+    this.user = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [
+        Validators.required, Validators.minLength(6)])
+    }, {
+      validators: this.repeatPasswordValidator.bind(this),
+    })
+  }
+
+  submit() {
+    if (this.user.valid) {
+      console.log(this.user.value)
+    } else {
+      console.log('Not valid')
+    }
+  }
+
+  repeatPasswordValidator(control: AbstractControl) {
+    return control.get('password')?.value === control.get('confirmPassword')?.value
+      ? null
+      : {mismatch: true}
+  }
 }
