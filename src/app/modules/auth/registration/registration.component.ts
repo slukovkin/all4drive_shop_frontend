8452fbb6e76a220a0cf7a307f25f748ee6c93c80
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-12 17:25:52 +0300

    created auth forms

--- src/app/modules/auth/registration/registration.component.ts
+++ src/app/modules/auth/registration/registration.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
