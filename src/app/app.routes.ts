4f064f4cb131b3410e382beeec9dc691c70cba4f
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-12 12:02:22 +0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./modules/auth/login/login.component";
+import {RegistrationComponent} from "./modules/auth/registration/registration.component";
+import {ProductListComponent} from "./modules/product/product-list/product-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LoginComponent,
+    title: 'Login'
+  },
+  {
+    path: 'registration', component: RegistrationComponent,
+    title: 'Registration'
+  },
+  {
+    path: 'products', component: ProductListComponent,
+    title: 'Products'
+  }
+];
